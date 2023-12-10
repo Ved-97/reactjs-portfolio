442c6e188a06c15651fcd7dbaae8cb6bc0f7041e
Vedant Chokshi <vedchokshi1597@gmail.com>
2023-12-10 07:17:45 -0500

    Project Setup Initial Files

---
+++ src/Pages/Home/HeroSection.jsx
@@ -0,0 +1,23 @@
+export default function HeroSection() {
+    return (
+       <section id="heroSection" className="hero--section">
+
+        <div className="hero--section--content-box">
+           <div className="hero--section-content">
+             <p className="section--title">Hey, I'm Vedant</p>
+             <h1 className="hero--section--title">
+                <span className="hero--section-title--color">
+                Front End
+                </span>{""}
+                <br />
+                Developer
+             </h1>
+                <p className="hero--section-description"> Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                <br /> Dolorum,sjdsadnadjsanajd?
+             </p>
+           </div>
+           <button className="btn btn-"></button>
+        </div>
+       </section>
+    );
+}
