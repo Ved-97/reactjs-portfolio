7014714624214d52f5b212b5429770916e332f1a
Vedant Chokshi <vedchokshi1597@gmail.com>
2023-12-18 18:00:53 -0500

    Update

--- src/Pages/Home/HeroSection.jsx
+++ src/Pages/Home/HeroSection.jsx
@@ -1,7 +1,7 @@
 export default function HeroSection() {
     return (
        <section id="heroSection" className="hero--section">
-        <div className="hero--section--content-box">
+        <div className="hero--section--content--box">
            <div className="hero--section-content">
              <p className="section--title">Hey, I'm Vedant</p>
              <h1 className="hero--section--title">
