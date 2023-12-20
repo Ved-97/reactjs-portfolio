2d2f8069879635d71ffbf327f12c6be822b93416
Vedant Chokshi <vedchokshi1597@gmail.com>
2023-12-20 07:39:30 -0500

    Navbar and Skills Update

--- src/Pages/Home/Navbar.jsx
+++ src/Pages/Home/Navbar.jsx
@@ -1,5 +1,5 @@
-import { useState, useEffect} from "react";
-import { Link } from "react-scroll";
+import { useState, useEffect} from 'react';
+import { Link } from 'react-scroll';
 
 function Navbar() {
     const [navActive, setNavActive] = useState(false);
@@ -37,7 +37,91 @@ function Navbar() {
         "active" : ""}`}>
             <div>
                 <img src="./img/logo.svg" alt="logoipsum" />
-            </div> 
+            </div>
+            <a className={`nav_hamburger ${navActive ?
+            "active" : ""}`}
+            onClick={togglenav}
+            >
+                <span className="nav__hamburger__line"> </span>
+                <span className="nav__hamburger__line"> </span>
+                <span className="nav__hamburger__line"> </span>
+            </a>
+            <div className={`navbar--items ${navActive ?
+            "active" : ""}`}>
+                <ul>
+                    <li>
+                        <Link 
+                            onClick={closeMenu} 
+                            activeClass="navbar--active-content"
+                            spy={true}
+                            smooth={true}
+                            offset={-70}
+                            duration={500}
+                            to="heroSection"
+                            className="navbar--content"
+                        >
+                            Home
+                        </Link>
+                    </li>
+
+                    <li>
+                        <Link 
+                            onClick={closeMenu} 
+                            activeClass="navbar--active-content"
+                            spy={true}
+                            smooth={true}
+                            offset={-70}
+                            duration={500}
+                            to="MyPortfolio"
+                            className="navbar--content"
+                        >
+                            Portfolio
+                        </Link>
+                    </li>
+
+                    <li>
+                        <Link 
+                            onClick={closeMenu} 
+                            activeClass="navbar--active-content"
+                            spy={true}
+                            smooth={true}
+                            offset={-70}
+                            duration={500}
+                            to="AboutMe"
+                            className="navbar--content"
+                        >
+                            About Me
+                        </Link>
+                    </li>
+
+                    <li>
+                        <Link 
+                            onClick={closeMenu} 
+                            activeClass="navbar--active-content"
+                            spy={true}
+                            smooth={true}
+                            offset={-70}
+                            duration={500}
+                            to="Testimonials"
+                            className="navbar--content"
+                        >
+                            Testimonials
+                        </Link>
+                    </li>
+
+                </ul>
+            </div>
+            <Link           
+                            onClick={closeMenu} 
+                            activeClass="navbar--active-content"
+                            spy={true}
+                            smooth={true}
+                            offset={-70}
+                            duration={500}
+                            to="Contact"
+                            className="btn btn-outline-primary"
+                            >Contact Me
+            </Link>
         </nav>
     );
 }
