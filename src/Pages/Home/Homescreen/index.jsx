0e6c215c348a0fcb00943afa36965b9da7f3b9f1
Vedant Chokshi <vedchokshi1597@gmail.com>
2023-12-23 17:09:22 -0500

    Update Portfolio

--- src/Pages/Home/Homescreen/index.jsx
+++ src/Pages/Home/Homescreen/index.jsx
@@ -1,11 +1,16 @@
+import AboutMe from "../AboutMe";
 import HeroSection from "../HeroSection";
+import MyPortfolio from "../MyPortfolio";
 import MySkills from "../MySkills";
 
 export default function Home() {
     return (
         <>
           <HeroSection />
+          <MyPortfolio/>
           <MySkills/>
+          <AboutMe />
+          
         </>
     );
 }
