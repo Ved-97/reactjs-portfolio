95204082a894a713e84e6c3c0bab496234b4efc1
Vedant Chokshi <vedchokshi1597@gmail.com>
2024-01-14 18:25:20 -0500

    Update

--- src/Pages/Home/HeroSection.jsx
+++ src/Pages/Home/HeroSection.jsx
@@ -10,8 +10,8 @@ export default function HeroSection() {
             Developer
           </h1>
           <p className="hero--section-description">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-            <br /> Dolorum, quas. Amet soluta assumenda cum?
+          With a knack for crafting seamless digital experiences. 
+            <br /> Transforming ideas into elegant code for a smarter, connected world.
           </p>
         </div>
         <button className="btn btn-primary">Get In Touch</button>
